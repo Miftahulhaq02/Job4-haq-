262668f69d56c112460a10a634977ac62a65c20c
Miftahulhaq02 <miftahhaq57@gmail.com>
2025-03-10 14:26:46 +0700

    Pesan Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <head>    
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Profil Saya</h1>
+                </div>
+                <div class="card-body">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiLyySSE5U6i1ikBYS5hp-pjvrarAxKqJQ_A&s" alt="Profile Picture" width="150">
+                    <h3>Muhammad Miftahul Haq</h3>
+                    <p>miftahhaq57@gmail.com</p>
+                    <p>"Bekerja keras, bermain lebih keras, dan selalu mencari cara untuk lebih berkembang."</p>
+                    <h5>Skills:</h5>
+                    <ul>
+                        <li>Pintar Ngaji</li>
+                        <li>Pintar Sholawat</li>
+                        <li>Bisa menjadi orang yang di ciptakan allah</li>
+                        <li>Bisa jadi umat Nabi Muhammad</li>
+                    <ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
